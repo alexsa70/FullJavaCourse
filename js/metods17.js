16d553c3e80bfc9b51259e79e3f02e01b625aa18
AlexL <alexl@gmail.com>
2022-01-07 11:13:48 +0200

    Methods17

---
+++ js/metods17.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const str = "teSt";
+
+//console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+//console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+//Вырезать world из фразы
+// console.log(logg.slice(6, 11));
+// console.log(logg.slice(-5, -1));
+// console.log(logg.slice(6).toUpperCase());
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substring(6, 9));
+
+//Math
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const numTest = "12.2px";
+//console.log(parseInt(numTest));
+console.log(parseFloat(numTest));
