3f84381e7f22e6e1646bfcd84e6a3cab46621a76
AlexL <alexl@gmail.com>
2022-01-08 15:51:37 +0200

    callback lesson 19

---
+++ js/callback_19.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function first() {
+  //Do something
+  setTimeout(function () {
+    console.log(1);
+  }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+  console.log(`I learn: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log('I past this lesson!');
+}
+
+learnJS('JavaScript', done);
