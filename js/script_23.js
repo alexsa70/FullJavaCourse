0a50f50c9c178cdc754066a381d32c508abedf19
AlexL <alexl@gmail.com>
2022-01-11 20:54:51 +0200

    practic_lesson 24 + lesson 23

---
+++ js/script_23.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// let str = "some";
+// let strObj = new String(str);
+// console.log(typeof (str));
+// console.log(typeof (strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function () {
+    console.log("Hello");
+  }
+};
+const jonh = Object.create(soldier);
+// const jonh = {
+//   health: 100
+// };
+//Old method
+// jonh.__proto__ = soldier;
+// console.log(jonh.armor);
+//New method
+//Object.setPrototypeOf(jonh, soldier);
+jonh.sayHello();
+
